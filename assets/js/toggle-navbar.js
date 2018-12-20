fd0fb1ed14cb774d26a61267ae4e3dd3e38225de
dbytecoderc <oparahdc@gmail.com>
2018-12-20 13:59:29 +0100

    Added description text on home page

--- assets/js/toggle-navbar.js
+++ assets/js/toggle-navbar.js
@@ -1,7 +1,6 @@
 let mainNav = document.getElementById('js--menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
-navBarToggle.addEventListener('click', function () {
-
+navBarToggle.addEventListener('click', () => {
   mainNav.classList.toggle('active');
 });
