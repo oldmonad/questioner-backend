e616ebfd2362dd3f4d4f92d4738d6202531149c7
dbytecoderc <oparahdc@gmail.com>
2019-01-03 18:18:21 +0100

    Added body parser

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
+import path from 'path';
 import dataStructureRoutes from './server/usingJsDataStructures/routes/routes';
 // import dbRoutes from './server/usingDB/routes/routes';
 
 dotenv.config();
-
+const defaultApi = process.env.API_BASE;
 const server = express();
 const router = express.Router();
 
@@ -22,27 +23,34 @@ dataStructureRoutes(router);
 
 // Log requests to the console
 server.use(logger('dev'));
-server.use(
-  bodyParser.urlencoded({
-    extended: false,
-  }),
-);
+
+// Parse incoming request data
 server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+  extended: false,
+}));
+server.use('/documentation', express.static('build'));
+server.use(express.static(path.join(__dirname, '/client/src/build')));
 
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-//   if (req.method === 'OPTIONS') {
-//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-//     return res.status(200).json({});
-//   }
-//   next();
-// });
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
 
-const defaultApi = process.env.API_BASE;
 
 // API Routes
 server.use(defaultApi, router);
+
+// Set up all default catch-all route that sends a message in JSON format
+server.get('*', (req, res) => res.status(404).send({
+  message: 'That route does not exist',
+}));
+
 
 const app = http.createServer(server);
 
