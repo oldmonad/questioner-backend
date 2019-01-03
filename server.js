9c4189663a6b8e3e39f3d415e2500553557419a6
dbytecoderc <oparahdc@gmail.com>
2019-01-03 17:39:13 +0100

    removed header modification

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable consistent-return */
 /* eslint-disable import/no-named-as-default-member */
 /* eslint-disable eol-last */
@@ -9,13 +10,7 @@ import bodyParser from 'body-parser';
 import dataStructureRoutes from './server/usingJsDataStructures/routes/routes';
 // import dbRoutes from './server/usingDB/routes/routes';
 
-
 dotenv.config();
-
-// Set up express app
-
-// const routes = process.env.TYPE === 'db' ? dbRoutes : dataStructureRoutes;
-
 
 const server = express();
 const router = express.Router();
@@ -23,32 +18,31 @@ const router = express.Router();
 // Port configuration
 const port = parseInt(process.env.PORT, 10) || 8000;
 
-
 dataStructureRoutes(router);
-
 
 // Log requests to the console
 server.use(logger('dev'));
-server.use(bodyParser.urlencoded({
-  extended: false,
-}));
+server.use(
+  bodyParser.urlencoded({
+    extended: false,
+  }),
+);
 server.use(bodyParser.json());
 
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
 
 const defaultApi = process.env.API_BASE;
 
 // API Routes
 server.use(defaultApi, router);
-
 
 const app = http.createServer(server);
 
