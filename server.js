4b2523fa4f65911e535348d9b96995e2b7523bbd
dbytecoderc <oparahdc@gmail.com>
2019-01-04 18:03:15 +0100

    updated babel setup for test suit

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * Adds commas to a number
+ * @param {number} number
+ * @param {string} locale
+ * @return {string}
+ */
+export default function (number, locale) {
+  return number.toLocaleString(locale);
+};
