758bb20d17419645468fe1ca9a44d1f1da67f4e1
dbytecoderc <oparahdc@gmail.com>
2019-01-07 08:22:23 +0100

    [Feature #163004650] Added logic for making an upvote

--- server/usingJsDataStructures/controller/vote.js
+++ server/usingJsDataStructures/controller/vote.js
@@ -4,6 +4,19 @@ import Storage from '../models/storage';
 
 const voteController = {
 
+  upvote(req, res) {
+    const question = parseInt(req.params.questionId);
+    // console.log(typeof (question));
+    const meetup = req.body.meetupId;
+    Storage.makeUpvote(meetup, question);
+    // console.log(upvote);
+    const votes = Storage.getVotes(meetup, question);
+    res.status(200).json({
+      status: 200,
+      data: votes,
+    });
+  },
+
 };
 
 export default voteController;
