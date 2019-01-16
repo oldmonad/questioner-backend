6201542cf20f326bf883d7ec78a414ab50b18c1d
dbytecoderc <oparahdc@gmail.com>
2019-01-16 11:19:24 +0100

    [feature #163224027] Added user model logic

--- server/models/models.js
+++ server/models/models.js
@@ -11,7 +11,36 @@ class UserModel {
     this.phonenumber = user.phonenumber;
   }
 
+  async signUp() {
+    const text = `INSERT INTO users (firstname, lastname,
+      username, email, password,  phonenumber)
+      VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+    const values = [this.firstname, this.lastname, this.username,
+      this.email, this.password, this.phonenumber,
+    ];
+    const {
+      rows,
+    } = await pool.query(text, values);
+    return rows[0];
+  }
 
+  static async findUserByEmail(email) {
+    const text = 'SELECT * FROM users WHERE email = $1';
+    const values = [email];
+    const {
+      rows,
+    } = await pool.query(text, values);
+    return rows[0];
+  }
+
+  static async findUserByUsername(username) {
+    const text = 'SELECT * FROM users WHERE username = $1';
+    const values = [username];
+    const {
+      rows,
+    } = await pool.query(text, values);
+    return rows[0];
+  }
 }
 
 export default UserModel;
