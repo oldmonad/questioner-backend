6256c75a8f7074b371005deae4dbdef3fd400efe
dbytecoderc <oparahdc@gmail.com>
2019-01-16 11:18:04 +0100

    [feature #163224027] Configured bcrypt library

--- server/utilities/bcrypt.js
+++ server/utilities/bcrypt.js
@@ -1,4 +1,13 @@
 /* eslint-disable eol-last */
 import bcrypt from 'bcrypt';
 
+const Bcrypt = {
+  hashPassword(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+  },
+
+  comparePassword(hashPassword, password) {
+    return bcrypt.compareSync(password, hashPassword);
+  },
+};
 export default Bcrypt;
