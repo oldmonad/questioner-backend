b99552a088a0359d3d149ab91df79d090e7f7466
dbytecoderc <oparahdc@gmail.com>
2019-01-17 11:05:13 +0100

    [Chore #163253712] Added test case to test for user signup and login

--- server/test/users.spec.js
+++ server/test/users.spec.js
@@ -41,3 +41,40 @@ describe('Create a user', async () => {
       });
   });
 });
+
+
+describe('Login an existing User', async () => {
+  it('Should Login an existing user', (done) => {
+    request(server)
+      .post('/api/v1/auth/login')
+      .send(userLogin)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.status).to.equal(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('token');
+        // console.log(res.body.token);
+        userToken = res.body.token;
+        return done();
+      });
+  });
+
+  it('Should throw an error if login is invalid', (done) => {
+    request(server)
+      .post('/api/v1/auth/login')
+      .send(invalidUser)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.status).to.equal(404);
+        expect(res.body).to.have.property('error');
+        expect(res.body.error).to.equal('The credentials you provided is incorrect');
+        return done();
+      });
+  });
+});
