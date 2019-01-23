5fdc56ca66b3647781de6fe0f993c3ca1392d98d
dbytecoderc <oparahdc@gmail.com>
2019-01-23 07:54:23 +0100

    [Chore #163415675] Added module for custom error messages

---
+++ server/utilities/errorresponses.js
@@ -0,0 +1,12 @@
+/* eslint-disable eol-last */
+const customErrorResponses = {
+  required: 'Please insert your :attribute.',
+  alpha: 'Your :attribute should contain only alphabets.',
+  min: 'Your :attribute should not be less than :min characters long.',
+  max: 'Your :attribute should not be more than :max characters long.',
+  email: 'Please insert a valid :attribute address.',
+  date: 'Please insert a valid date and time',
+  alpha_num: 'Your :attribute should contain only alphabets and numbers',
+};
+
+export default customErrorResponses;
