47d0e53f081f5ab4b10baac95f90452a3a413c0a
dbytecoderc <oparahdc@gmail.com>
2019-01-23 09:26:19 +0100

    [Chore #163415675] Refactored the sign up controller to use custom error formatter

--- server/app.js
+++ server/app.js
@@ -8,14 +8,9 @@ import bodyParser from 'body-parser';
 // Set up express app
 const app = express();
 import routes from './routes';
-// import userRoutes from './routes/user';
-// import meetupRoutes from './routes/meetups';
-// import questionsRoute from './routes/questions';
-// import commentRoutes from './routes/comments';
 
 // Log requests to the console
 app.use(logger('dev'));
-
 
 // Parse incoming request data
 app.use(bodyParser.json());
@@ -24,10 +19,6 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use('/api/v1', routes);
-// app.use('/api/v1', userRoutes);
-// app.use('/api/v1/meetups', meetupRoutes);
-// app.use('/api/v1/questions', questionsRoute);
-// app.use('api/v1/comments', commentRoutes);
 
 app.use((req, res, next) => {
   const error = new Error('Invalid route');
--- server/controller/user.js
+++ server/controller/user.js
@@ -2,42 +2,33 @@
 import jwt from '../utilities/jwt';
 import bcrypt from '../utilities/bcrypt';
 import UserModel from '../models/user';
+import {
+  successResponse,
+  errorResponse,
+} from '../utilities/responseformat';
 
 
 const UserController = {
   async createNewUser(req, res) {
     if (req.body.password !== req.body.confirmPassword) {
-      return res.status(400).json({
-        status: 400,
-        error: 'Passwords do not match',
-      });
+      return errorResponse(res, 400, 'Passwords do not match');
     }
 
     const newUser = new UserModel(req.body);
     const isExistingUserMail = await UserModel.findUserByEmail(newUser.email);
     if (isExistingUserMail) {
-      return res.status(409).json({
-        status: 409,
-        error: 'This email address is already taken',
-      });
+      return errorResponse(res, 409, 'This email address is not available');
     }
 
     const isExistingUsername = await UserModel.findUserByUsername(newUser.username);
     if (isExistingUsername) {
-      return res.status(409).json({
-        status: 409,
-        error: 'This username is already taken',
-      });
+      return errorResponse(res, 409, 'This username is not available')
     }
     newUser.password = bcrypt.hashPassword(newUser.password);
 
     const createdUser = await newUser.newUserSignUp();
 
-    return res.status(201).json({
-      status: 201,
-      message: 'User created',
-      data: createdUser,
-    });
+    return successResponse(res, 201, 'User Created', createdUser);
   },
 
   async loginUser(req, res) {
