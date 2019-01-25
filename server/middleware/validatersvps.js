942ea169daaf738cff26d22841d5e4d1dade58db
dbytecoderc <oparahdc@gmail.com>
2019-01-25 08:48:23 +0100

    [Chore #163453688] added validation for rsvps

---
+++ server/middleware/validatersvps.js
@@ -0,0 +1,28 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable eol-last */
+import Validator from 'validatorjs';
+import {
+  errorResponse,
+} from '../utilities/responseformat';
+import customErrorMessages from '../utilities/errorresponses';
+
+export default class RsvpValidation {
+  static validRsvp(req, res, next) {
+    const userResponse = req.body;
+    if (userResponse.response) {
+      userResponse.response = userResponse.response.trim().toLowerCase();
+    }
+
+    const responseProperties = {
+      response: ['required', { in: ['yes', 'no', 'maybe']
+      }],
+    };
+
+    const validator = new Validator(userResponse, responseProperties);
+    validator.passes(() => next());
+    validator.fails(() => {
+      const errorMessage = 'Your response should be either Yes, No, or Maybe';
+      return errorResponse(res, 400, errorMessage);
+    });
+  }
+}
