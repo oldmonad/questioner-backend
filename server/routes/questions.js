46d77f3242a6ca3805cafe3a77c6364a21ea131c
Oparah DC <oparahdc@gmail.com>
2019-01-31 14:00:38 +0100

    Update questions.js

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -10,7 +10,6 @@ import tryCatch from '../utilities/trycatch';
 
 
 router.post('/', validate.validateQuestion, tryCatch(questionsController.createQuestion));
-
 router.patch('/:id/upvote', tryCatch(questionsController.upvote));
 router.patch('/:id/downvote', tryCatch(questionsController.downvote));
 
