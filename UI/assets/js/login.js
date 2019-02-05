393c171341a92c1b1fe3aa48dfb92bd6792afb87
dbytecoderc <oparahdc@gmail.com>
2019-02-05 11:31:48 +0100

    [Chore #163649571] Added backed up files

---
+++ UI/assets/js/login.js
@@ -0,0 +1,91 @@
+const windowUrlArray = window.location.href.split('/');
+windowUrlArray.pop();
+const windowUrl = windowUrlArray.join('/');
+const apiUrl = 'https://enigmatic-refuge-95413.herokuapp.com/api/v1/auth/login';
+const loginForm = document.querySelector('.custom--form');
+const e_mail = document.querySelector('#email');
+const pass_word = document.querySelector('#password');
+const submitBtn = document.querySelector('#submit');
+
+
+const showOverlay = () => {
+  document.querySelector('.loading').style.display = 'block';
+};
+
+const hideOverlay = () => {
+  document.querySelector('.loading').style.display = 'none';
+};
+
+
+function showAlert(message, className) {
+  const div = document.createElement('div');
+  //Add ClassName
+  div.className = `alert ${className}`;
+  const p = document.createElement("p");
+  p.appendChild(document.createTextNode(message));
+  div.appendChild(p);
+
+  const container = document.querySelector('.login--section');
+  const form = document.querySelector('.custom--form');
+
+  //Insert alert
+  container.insertBefore(div, form);
+  //Timeout after 5 seconds
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 5000);
+}
+
+
+const login = async (e) => {
+  e.preventDefault();
+  showOverlay();
+  const email = e_mail.value;
+  const password = pass_word.value;
+
+  const userLogin = {
+    email,
+    password
+  };
+
+  await fetch(apiUrl, {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify(userLogin),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(res => res.json())
+    .then((response) => {
+      hideOverlay();
+
+      if (response.status === 400) {
+        return showAlert('Incorrect email or password', 'error');
+      }
+      if (response.status === 401) {
+        return showAlert('Incorrect email or password', 'error');
+      }
+
+      if (response.status === 200) {
+        showAlert(`${response.message}`, 'success');
+        const {
+          data,
+        } = response;
+
+        localStorage.setItem('user', JSON.stringify(data));
+
+        if (data.admin === true) {
+          window.location.href = `${windowUrl}/admin_meetups.html`;
+        } else {
+          window.location.href = `${windowUrl}/user_meetups.html`;
+        }
+      }
+      return true;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+loginForm.addEventListener('submit', login);
--- UI/assets/js/signup.js
+++ UI/assets/js/signup.js
@@ -1,19 +1,133 @@
-function showLoader() {
-  document.querySelector('.loading').style.display = 'block';
+const apiUrl = 'https://enigmatic-refuge-95413.herokuapp.com/api/v1/auth/signup';
+const windowUrlArray = window.location.href.split('/');
+windowUrlArray.pop();
+const windowUrl = windowUrlArray.join('/');
+const first_name = document.querySelector('#first-name');
+const last_name = document.querySelector('#last-name');
+const e_mail = document.querySelector('#email');
+const phone_number = document.querySelector('#phone-number');
+const user_name = document.querySelector('#user-name');
+const pass_word = document.querySelector('#password');
+const confirm_password = document.querySelector('#confirm-password');
+
+//NOT YET FUNCTIONAL
+const inputFields = [
+  first_name, last_name, e_mail, phone_number,
+  user_name, pass_word, confirm_password,
+];
+
+
+function showAlert(message, className) {
+  const div = document.createElement('div');
+  //Add ClassName
+  div.className = `alert ${className}`;
+  const p = document.createElement("p");
+  p.appendChild(document.createTextNode(message));
+  div.appendChild(p);
+
+  const container = document.querySelector('.signup--section');
+  const form = document.querySelector('.signup--form');
+
+  //Insert alert
+  container.insertBefore(div, form);
+  //Timeout after 5 seconds
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 5000);
 }
 
-function hideLoader() {
-  document.querySelector('.loading').style.display = 'none';
+function clearAlert() {
+  document.getElementById('number').value = '';
 }
 
+//NOT YET FUNCTIONAL
+const removeClass = (errorclass) => {
+  const errorText = document.querySelector(errorclass);
+  if (errorText) {
+    errorText.display.hidden = true;
+    errorText.remove();
+  }
+};
 
-const url = 'https://enigmatic-refuge-95413.herokuapp.com/auth/signup';
 
-function resetInputFields() {
-  const InputFields = document.querySelectorAll('.error');
-  const InputFieldsArray = Array.prototype.slice.call(InputFields);
-  InputFieldsArray.forEach((element) => {
-    const currentField = element;
-    currentField.innerHTML = '';
+//NOT YET FUNCTIONAL
+const inputErrorHandler = (errorArray, errorData) => {
+  errorArray.forEach((errorKey) => {
+    const errorId = `${errorKey}`;
+    const checkFieldError = inputFields.find(field => field.id === errorId);
+    removeClass(`.${errorId}`);
+    checkFieldError.insertAdjacentHTML('afterend', `
+    <p class="${errorId}" id="red">${errorData[errorId][0]}</p><br>
+    `);
   });
-}
+};
+
+//NOT YET FUNCTIONAL
+const emailErrorHandler = (errorData) => {
+  removeClass('.emailerror');
+  emailInput.insertAdjacentHTML('afterend', `
+  <p class="emailerror" id="red">${errorData}</p><br>
+  `);
+};
+//NOT YET FUNCTIONAL
+const usernameErrorHandler = (errorData) => {
+  removeClass('.usernameerror');
+  usernameInput.insertAdjacentHTML('afterend', `
+  <p class="usernameerror" id="red">${errorData}</p><br>
+  `);
+};
+
+
+const register = async (e) => {
+  e.preventDefault();
+  // resetInputFields();
+  // showLoader();
+
+
+  const firstname = first_name.value;
+  const lastname = last_name.value;
+  const email = e_mail.value;
+  const phonenumber = phone_number.value;
+  const username = user_name.value;
+  const password = pass_word.value;
+  const confirmPassword = confirm_password.value;
+
+  const userInput = {
+    firstname,
+    lastname,
+    username,
+    email,
+    password,
+    confirmPassword,
+    phonenumber,
+  };
+
+  await fetch(apiUrl, {
+      method: 'POST',
+      body: JSON.stringify(userInput),
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(res => res.json())
+    .then((data) => {
+      if (data.status !== 201) {
+        console.log(data.error);
+        showAlert('Some inputs are incorrect check console for directives this is not a permanent fix', 'error');
+      }
+
+      if (data.status === 201) {
+        showAlert('Account created successfully you would be redirected to the login page', 'success');
+        setTimeout(() => {
+          window.location.href = `${windowUrl}/login.html`;
+        }, 5000);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const signup = document.querySelector('#signup--form');
+signup.addEventListener('submit', register);
